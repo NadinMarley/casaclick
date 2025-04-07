c78e5d7a1d07b8029dca2b3093e35e5bf1fabbfe
NadinMarley <110132791+NadinMarley@users.noreply.github.com>
2025-04-06 23:09:40 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("¡VanessaBot está en línea!");
+});
+
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+ if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`
+Servidor corriendo en el puerto ${PORT}`);
+});
